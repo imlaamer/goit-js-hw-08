00b4773c395608bdc457a3283e94a8602b208726
imlaamer <lolitayenik@ukr.net>
2023-10-22 00:18:42 +0300

    1 and 2hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+let markup = "";
+galleryItems.forEach(
+  ({ preview, original, description }) =>
+    (markup += `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" width="300" />
+        </a>
+      </li>`)
+);
+
+const galleryList = document.querySelector(".gallery");
+
+galleryList.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.98,
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_TIME = 'videoplayer-current-time';
+const iframeElem = document.querySelector('#vimeo-player');
+const player = new Player(iframeElem);
+
+
+const onPlay = throttle(function (data) {
+  localStorage.setItem(KEY_TIME, JSON.stringify(data.seconds));
+}, 1000);
+player.on('timeupdate', onPlay, { passive: false });
+
+const storageCurrentTime = JSON.parse(localStorage.getItem(KEY_TIME)) ?? 0;
+
+player
+  .setCurrentTime(storageCurrentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+
+//   Що робить this код?
+
+// { passive: false }
+
+// switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
